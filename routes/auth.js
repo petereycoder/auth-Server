4af0456ae90f16c5bbb8ef9fcc548a6daa11819e
petereycoder <fp.pipe.reyes@gmail.com>
2022-03-15 20:59:00 -0600

    MEAN

---
+++ routes/auth.js
@@ -0,0 +1,34 @@
+const { Router } = require('express');
+
+
+const router = Router();
+
+//Crear un nuevo usuario
+router.post( '/new', (req, res) => {
+
+    return res.json({
+        ok: true,
+        msg: 'Crear usuario /new'
+    });
+
+});
+
+//Login de usuario
+router.post( '/', (req, res) => {
+
+    return res.json({
+        ok: true,
+        msg: 'Login de  usuario /'
+    });
+
+});
+
+//Validar y revalidar token
+router.get( '/renew', (req, res) => {
+
+    return res.json({
+        ok: true,
+        msg: 'Renew'
+    });
+
+});
